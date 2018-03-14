505944ff54bd60c4de0a0ed4a9a28cb979e3d4a1
Denis Bogdanov <bogdanov.math@gmail.com>
2018-03-15 00:19:44 +0300

    Add DI examples from Kevin Bowersox for Collections.

---
+++ xml-configuration/src/main/java/ru/bogdaniium/xml_configuration/kevin/App.java
@@ -0,0 +1,17 @@
+package ru.bogdaniium.xml_configuration.kevin;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("kevin-application-context.xml");
+
+        Bean bean = context.getBean("bean", Bean.class);
+
+        System.out.println(bean.getList());
+        System.out.println(bean.getSet());
+        System.out.println(bean.getMap());
+    }
+}
---
+++ xml-configuration/src/main/java/ru/bogdaniium/xml_configuration/kevin/Bean.java
@@ -0,0 +1,41 @@
+package ru.bogdaniium.xml_configuration.kevin;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Bean {
+
+    private List<String> list;
+
+    private Map<String, String> map;
+
+    private Set<String> set;
+
+    public Bean() {
+    }
+
+    public List<String> getList() {
+        return list;
+    }
+
+    public void setList(List<String> list) {
+        this.list = list;
+    }
+
+    public Map<String, String> getMap() {
+        return map;
+    }
+
+    public void setMap(Map<String, String> map) {
+        this.map = map;
+    }
+
+    public Set<String> getSet() {
+        return set;
+    }
+
+    public void setSet(Set<String> set) {
+        this.set = set;
+    }
+}
